0dadb2c99e79d93ff39d3b138eac6fe9dd25f2ba
Commander.Alchemy <Commander.Alchemy@gmail.com>
2014-09-12 18:08:43 +0200

    Modified g9x for new USB port, and other.

--- g9x.sh
+++ g9x.sh
@@ -2,4 +2,4 @@
 #
 # Change mouse speed with constant deceleration to use with high DPI.
 
-xinput set-prop 10 "Device Accel Constant Deceleration" 4
+xinput set-prop 8 "Device Accel Constant Deceleration" 4
