882dc67b8988f452cb56120d14ccc32affe4d535
Commander.Alchemy <Commander.Alchemy@gmail.com>
2014-10-12 19:52:48 +0200

    Added automatic detection for g9x and set acceleration values for ID's found

--- g9x.sh
+++ g9x.sh
@@ -2,4 +2,9 @@
 #
 # Change mouse speed with constant deceleration to use with high DPI.
 
-xinput set-prop 8 "Device Accel Constant Deceleration" 4
+mouse_id=($(xinput list | awk -F "[=\t]" '/G9x/ {print $3}'))
+
+for i in "${mouse_id[@]}"; do
+  echo "Setting Deceleration 4, for id: "$i""
+  xinput set-prop $i "Device Accel Constant Deceleration" 4
+done
